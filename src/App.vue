58e151b6381633bb47b80183bc5272520461a5bc
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-11-16 20:28:13 -0300

    2/3-almost-done

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,191 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+
+  <v-app>
+    <div>
+    <v-system-bar
+      window
+      dark
+    >
+      <v-icon>mdi-message</v-icon>
+      <span>10 unread messages</span>
+      <v-spacer></v-spacer>
+      <v-icon>mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
   </div>
+  <v-app-bar
+      relative  
+      color="white"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <!-- Sizes your content based upon application components -->
+  <v-main>
+<v-content>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum rem suscipit magnam molestias
+       quos aspernatur optio delectus fuga sint accusantium eaque veniam aut consectetur labore,
+        incidunt dolor quibusdam rerum quas.
+      <!-- If using vue-router -->
+      <!-- <router-view></router-view> -->
+    </v-content>
+    <!-- Provides the application the proper gutter -->
+
+  </v-main>
+  <v-navigation-drawer
+      permanent
+      width="20%"
+    >
+      <v-row
+        class="fill-height"
+        no-gutters
+      >
+        <v-navigation-drawer
+          dark
+          mini-variant
+          mini-variant-width="56"
+          permanent
+        >
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/women/75.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-divider></v-divider>
+
+          <v-list
+            dense
+            nav
+          >
+            <v-list-item
+              v-for="item in items"
+              :key="item.title"
+            >
+              <v-list-item-action>
+                <v-icon>{{ item.icon }}</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+
+        <v-list class="grow">
+          <v-list-item
+            v-for="link in links"
+            :key="link"
+            link
+          >
+            <v-list-item-title v-text="link"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-row>
+    </v-navigation-drawer>
+    
+  
+
+  
+
+  
+
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex"
+      flat
+      tile
+    >
+      <v-card-title class="teal">
+        <strong class="subheading">Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-2"
+          dark
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text class="py-2 white--text text-center">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+
+    <v-bottom-navigation
+    :value="value"
+    color="primary"
+  >
+    <v-btn>
+      <span>Recents</span>
+
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Favorites</span>
+
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <span>Nearby</span>
+
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+
+</v-app>
 </template>
+<script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: 'App',
+  data(){
+    return {
+      items: [
+          { title: 'Home', icon: 'mdi-view-dashboard' },
+          { title: 'About', icon: 'mdi-forum' },
+        ],
+         icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+        links: ['Home', 'Contacts', 'Settings'],
+        mini: true,
+    }
+  }
+};
+</script>
